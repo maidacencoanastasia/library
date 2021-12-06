e2820b814e6f8c552cbc0dbc34997bd084039069
maidacencoanastasia <maidacenco100@gmail.com>
2021-12-06 22:27:57 +0200

    Add Controller and Repository

---
+++ src/main/java/com/library/library/spring/repository/GenreRepository.java
@@ -0,0 +1,14 @@
+package com.library.library.spring.repository;
+
+import com.library.library.domain.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+
+    List<Genre> findByNameContainingIgnoreCaseOrderByName(String name);
+
+}
---
+++ src/main/java/com/library/library/spring/repository/PublisherRepository.java
@@ -0,0 +1,14 @@
+package com.library.library.spring.repository;
+
+import com.library.library.domain.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
+    List<Publisher> findByNameContainingIgnoreCaseOrderByName(String name);
+
+}
