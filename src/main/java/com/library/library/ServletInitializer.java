4d2506d12d6af2deef87ab94e8d5f9f1e26cd131
maidacencoanastasia <maidacenco100@gmail.com>
2021-12-06 21:06:12 +0200

    Add main classes

--- src/main/java/com/library/library/ServletInitializer.java
+++ src/main/java/com/library/library/ServletInitializer.java
@@ -1,13 +1,14 @@
 package com.library.library;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+
+@SpringBootApplication
+@EnableAspectJAutoProxy // включаем использование AspectJ
 public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(LibraryApplication.class);
-	}
 
 }
